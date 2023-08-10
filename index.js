046fa94797023e14da4445217129aa127502bb76
jeffmanhart <jeffreymanhart@gmail.com>
2023-08-10 12:39:56 -0600

    Created a countdown loop and a card writing loop

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    const messages = [];
+    for (let i = 0; i< names.length; i++){
+        messages[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        
+    }
+    return messages;
+}
+function countDown(num){
+    let i = 0;
+    while (i <= num) {
+        console.log(num--);
+    }
+}
